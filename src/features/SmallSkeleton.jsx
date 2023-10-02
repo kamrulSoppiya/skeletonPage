adc9ea902a880e8d01ec785c5b6caa88ea63b65f
kamrulSoppiya <kamrul.soppiya@gmail.com>
2023-10-02 17:10:50 +0600

    small Skeleton panding

--- src/features/SmallSkeleton.jsx
+++ src/features/SmallSkeleton.jsx
@@ -1,9 +1,12 @@
-const SmallSkeleton = () => {
-  return (
-    <div>
-      
+import { useEffect, useState } from 'react';
+import classes from '../assets/smallSkeleton.module.scss';
+
+export const SmallSkeleton = ({url, skeletonRows})=>{
+
+  return(
+    <div className="container">
+
     </div>
-  )
+  );
 }
 
-export default SmallSkeleton;
